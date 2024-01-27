8b94e5362e96585d7df3d32a24ed2dcb812189b7
Dev_02 <Dev02@gmail.com>
2024-01-27 14:24:51 +0000

    Dev_02 has been added print stmt

---
+++ Parallel_Development_Conflicts.py
@@ -0,0 +1,5 @@
+# Here test case to observ a parallel development while working two developers with same file name and same line
+# Lets observ the what are the conflicts we are going to face while github commits
+
+# Please print your User name first
+print("This is Developer 02 User")

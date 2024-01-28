1374f2d6cd8f372da5d18e57b0735b5c28f8d29f
Cloud-Karthik <karthiksandiri23@gmail.com>
2024-01-28 20:47:55 +0530

    bugs are fixed in python script

--- Development.py
+++ Development.py
@@ -3,3 +3,6 @@
 print("This is Development Branch Code")
 print("issue has been fixed")
 print("Checking the difference between master branch and release brache")
+
+# working on bug fixes on this file and stash the another release working files from staging area
+
